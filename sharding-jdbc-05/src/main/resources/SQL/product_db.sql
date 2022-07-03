f4856269fe26108bc72dfa71154bbae89e8cef5a
cjb <18796327106@163.com>
2022-07-03 15:43:15 +0800

    sharding-jdbc读写分离

---
+++ sharding-jdbc-05/src/main/resources/SQL/product_db.sql
@@ -0,0 +1,55 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 192.168.47.153-master
+ Source Server Type    : MySQL
+ Source Server Version : 50726
+ Source Host           : 192.168.47.153:3306
+ Source Schema         : product_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 50726
+ File Encoding         : 65001
+
+ Date: 03/07/2022 13:41:18
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for product_base
+-- ----------------------------
+DROP TABLE IF EXISTS `product_base`;
+CREATE TABLE `product_base`  (
+  `product_id` bigint(20) NOT NULL COMMENT '商品ID',
+  `shop_id` bigint(20) NULL DEFAULT NULL COMMENT '店铺ID',
+  `name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '名称',
+  `type_id` bigint(20) NULL DEFAULT NULL,
+  `price` bigint(20) NULL DEFAULT NULL COMMENT '价格',
+  `origin_address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '产地',
+  PRIMARY KEY (`product_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for product_description
+-- ----------------------------
+DROP TABLE IF EXISTS `product_description`;
+CREATE TABLE `product_description`  (
+  `product_id` bigint(20) NOT NULL COMMENT '商品ID',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '描述内容',
+  `shop_id` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`product_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for product_type
+-- ----------------------------
+DROP TABLE IF EXISTS `product_type`;
+CREATE TABLE `product_type`  (
+  `type_id` bigint(20) NOT NULL COMMENT '商品类型ID',
+  `type_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '商品类型名称',
+  PRIMARY KEY (`type_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
